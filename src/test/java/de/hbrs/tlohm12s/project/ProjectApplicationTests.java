5a3584409ff9f2d7db70aa9c5f9969c9dea73b3e
Tobias Lm <tobilo992@gmail.com>
2021-02-04 15:37:19 +0100

    First commit

---
+++ src/test/java/de/hbrs/tlohm12s/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hbrs.tlohm12s.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
